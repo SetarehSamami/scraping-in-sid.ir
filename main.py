bb456dca5b26f170763a1b40de6c6b90a2bdadfa
setare <samamiset@gmail.com>
2023-07-28 03:25:44 +0330

    adding url and page and save address

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
 
-links=[]
+'''information'''
 
-for page in range(1, 3):
-     url = "https://sid.ir/search/paper/%D8%B1%D9%88%D8%A7%D9%86%D8%B4%D9%86%D8%A7%D8%B3%DB%8C/fa/" + "?page=" +str(page)+"&sort=1&ftyp=all&fgrp=all&fyrs=1379%2c1402"
+url_='https://sid.ir/search/paper/%D8%B1%D9%88%D8%A7%D9%86%D8%B4%D9%86%D8%A7%D8%B3%DB%8C/fa/?page=1&sort=1&ftyp=all&fgrp=all&fyrs=1379%2c1402'
+pages = 1
+adr_='C:/Users/lenovo/Desktop/BN/web scraping'
+
+
+'''pdf Download link'''
+
+links=[]
+for page in range(1, pages+1 ):
+
+     l = tuple(url_.split('page=1'))
+     url=(l[0] + 'page=' + str(page) + l[1])
+
      furl = requests.get(url)
      jsoup = BeautifulSoup(furl.text , 'html.parser')
      productslist=[]
      for products in jsoup.find_all('p', class_="srtitle" ):
           products = products.find('a').get('href')
           p = productslist.append(products)
+
 
 
      for web in productslist:
@@ -29,10 +41,12 @@ for page in range(1, 3):
 
 
 '''download'''
+
 i=1
 for l in links:
       response=requests.get(l)
-      adr='pdf/%s.pdf' %i
+      adr='/%s.pdf' %i
+      adr=adr_+adr
       with open(adr, 'wb') as f:
           f.write(response.content)
       i=i+1
